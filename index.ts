acbcc585766ea2e81009eefb97248d490b5c5506
Dennis Bartlett <dbartlett@shiroi.attlocal.net>
2021-03-21 17:19:58 -0400

    Finished Typescript conversion

--- index.ts
+++ index.ts
@@ -1,21 +1,30 @@
-const { HttpPublisher } = require("electron-publish/out/publisher");
-const { httpExecutor } = require("builder-util/out/nodeHttpExecutor");
-const mime = require("mime");
-const { configureRequestOptions } = require("builder-util-runtime");
-const { Arch } = require("builder-util");
-const { createReadStream, stat } = require("fs-extra-p");
-const { basename, join } = require("path");
+import { Publisher, PublishContext, UploadTask } from "electron-publish/out/publisher";
 
 console.debug = function(...args) {
   if(process.env.DEBUG) console.log(`\x1b[33m[DEBUG] \x1b[0m`, ...args);
 }
 
-class Publisher extends HttpPublisher {
-  async upload(task) {
-    console.debug(task);
+class NucleusPublisher extends Publisher {
+  protected readonly context: PublishContext;
+  providerName = 'Nucleus';
+
+  constructor(context: PublishContext) {
+    super(context);
+    this.context = context;
   }
+
+  async upload(task: UploadTask) {
+    // this.createProgressBar();
+    console.debug(task);
+    // return new Promise ((resolve, reject) => {
+    //   resolve("done");
+    //   reject(new Error("…"));
+    // });
+  }
+
+  toString(): string {
+    return 'Not Implemented';
+  };
 }
   
-module.exports = {
-  default: Publisher
-};
+export default NucleusPublisher;
